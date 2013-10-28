b5995fe0ee20360fbebfa588d1e1c77763325696
Matt Frisbie <msfrisbie@gmail.com>
2013-10-27 20:07:22 -0700

    refactor event filter

--- app/assets/javascripts/filters/eventfilter.js
+++ app/assets/javascripts/filters/eventfilter.js
@@ -5,6 +5,14 @@ window.angular.module('Outhouse.events.filter', [])
     return function(event, setTime) {
       // return 'it worked!'+input;
 
+      var isBetween = function(time,start,end) {
+        return (time >= start && time <= end) ;
+      }
+
+      var dateFromUnix = function(unixtime) {
+        return new Date(unixtime*1000);
+      }
+
       if (!event) {
         return '';
       }
@@ -24,10 +32,6 @@ window.angular.module('Outhouse.events.filter', [])
         // time ranges exist, this event is non-recurring
         event.time_ranges.forEach(function(range) {
           if (setTime >= range.start && setTime <= range.end) {
-            // console.log("Open from " + 
-            //         new Date(range.start*1000).toLocaleTimeString() +
-            //         " to " + 
-            //         new Date(range.end*1000).toLocaleTimeString())
             str = ("" + 
                     new Date(range.start*1000).toLocaleTimeString() +
                     " - " + 
@@ -36,50 +40,31 @@ window.angular.module('Outhouse.events.filter', [])
         })
       } else {
         // this event recurs weekly
+        if (!event.schedule.time_range) {
+          event.schedule.time_range = [];
+        }
+
         var dateSet = {
           "date": new Date(setTime*1000)
         };
-        // dateSet['base100'] = dateSet.date.getHours()*100 + dateSet.date.getMinutes()*5/3;
+
         dateSet['base100'] = dateSet.date.getHours()*100 + dateSet.date.getMinutes();
 
         event.schedule[["sun","mon","tue","wed","thu","fri","sat"][dateSet.date.getDay()]].forEach(function(range) {
-          if (dateSet.base100 >= range.start && dateSet.base100 <= range.end) {
-            // if (!event.schedule.time_range) {
-            //   // JANK JANK JANK JANK JANK
-            //   str = "" + 
-            //           new Date(new Date(new Date(setTime*1000).setHours(range.start/100)).setMinutes((range.start%100)*3/5)).toLocaleTimeString() +
-            //           " - " + 
-            //           new Date(new Date(new Date(setTime*1000).setHours(range.end/100)).setMinutes((range.end%100)*3/5)).toLocaleTimeString()
-
-            // } else {
-            //   if (setTime >= event.schedule.time_range.start && setTime <= event.schedule.time_range.end) {
-            //     str = "" + 
-            //             new Date(new Date(new Date(setTime*1000).setHours(range.start/100)).setMinutes((range.start%100)*3/5)).toLocaleTimeString() +
-            //             " - " + 
-            //             new Date(new Date(new Date(setTime*1000).setHours(range.end/100)).setMinutes((range.end%100)*3/5)).toLocaleTimeString() 
-            //   }
-            // }
-            if (!event.schedule.time_range) {
-              // JANK JANK JANK JANK JANK
-              str = "" + 
-                      new Date(new Date(new Date(setTime*1000).setHours(range.start/100)).setMinutes((range.start%100))).toLocaleTimeString() +
-                      " - " + 
-                      new Date(new Date(new Date(setTime*1000).setHours(range.end/100)).setMinutes((range.end%100))).toLocaleTimeString()
-
-            } else {
-              if (setTime >= event.schedule.time_range.start && setTime <= event.schedule.time_range.end) {
-                str = "" + 
-                        new Date(new Date(new Date(setTime*1000).setHours(range.start/100)).setMinutes((range.start%100))).toLocaleTimeString() +
-                        " - " + 
-                        new Date(new Date(new Date(setTime*1000).setHours(range.end/100)).setMinutes((range.end%100))).toLocaleTimeString() 
-              }
-            }
+          if ((dateSet.base100 >= range.start 
+              && dateSet.base100 <= range.end) 
+            && ((event.schedule.time_range.length==0) 
+              || (setTime >= event.schedule.time_range.start 
+                && setTime <= event.schedule.time_range.end))) {
+            str = "" + 
+                  new Date(new Date(new Date(setTime*1000).setHours(range.start/100)).setMinutes((range.start%100))).toLocaleTimeString() +
+                  " - " + 
+                  new Date(new Date(new Date(setTime*1000).setHours(range.end/100)).setMinutes((range.end%100))).toLocaleTimeString() 
           }
         })
       }
       return str;
       console.log('This should never be reached!');
-      // return new Date(input*1000).toLocaleTimeString();
     }
   })
   .filter('stripSeconds', function() {
